eeefbc351dc67bc82442f86d456e17565cf811d9
Justin <zertukis@gmail.com>
2014-04-17 18:53:32 -0500

    random url function improved, less broken images

--- main.js
+++ main.js
@@ -7,24 +7,25 @@ window.onload = function() {
 		return Math.random().toString(36).slice(8);
 	}
 
+	function randomString() {
+    charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var randomString = '';
+    for (var i = 0; i < 5; i++) {
+    	var randomPoz = Math.floor(Math.random() * charSet.length);
+    	randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
+
 	var loadImg = function() {
 		for (i = 0; i < 20; i++) {
 
 			var images = document.createElement('img');
-			images.setAttribute('src', 'http://i.imgur.com/' + randomURL() + '.png');
+			images.setAttribute('src', 'http://i.imgur.com/' + randomString() + '.png');
 			div.appendChild(images);
 		}
-	}
-
-	
-		
+	}		
 
 	loadImg();
-
-
-		
-
-    
-
-
+	
 };
