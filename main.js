26abe9b9e96e05c6ac5dbd9f729e4c2d7206254b
Justin <zertukis@gmail.com>
2014-04-17 19:50:12 -0500

    function to load more after half point added, infinite scroll

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 window.onload = function() {
 
 	var div = document.getElementById('imgur');
-
-	var randomURL = function() {	
-
-		return Math.random().toString(36).slice(8);
-	}
-
+	
+	//generates random 5 letter string to use in the url
 	function randomString() {
-    charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var randomString = '';
-    for (var i = 0; i < 5; i++) {
-    	var randomPoz = Math.floor(Math.random() * charSet.length);
-    	randomString += charSet.substring(randomPoz,randomPoz+1);
+    	charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    	var randomString = '';
+    	for (var i = 0; i < 5; i++) {
+    		var randomPoz = Math.floor(Math.random() * charSet.length);
+    		randomString += charSet.substring(randomPoz,randomPoz+1);
     }
     return randomString;
-}
+	}
 
+	//creates img elements and adds url from randomString
 	var loadImg = function() {
 		for (i = 0; i < 20; i++) {
-
 			var images = document.createElement('img');
 			images.setAttribute('src', 'http://i.imgur.com/' + randomString() + '.png');
 			div.appendChild(images);
 		}
 	}		
 
-	loadImg();
-	
+	//loads more images when scrolled past half way point, allows infinite scroll
+	$(function(){
+   		$(window).scroll(function(){
+       		if($(window).scrollTop() > $(window).height()/2){
+           		loadImg();
+    	   	}
+   		});
+	});
+
 };
